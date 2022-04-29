0bc7c4e8afbac634740e4adf50cd44dd839ed6e6
Hiren <hirenbavaskar@gmail.com>
2022-04-29 06:53:24 +0530

    Updated register, course

--- routes/course.js
+++ routes/course.js
@@ -5,16 +5,15 @@ const client = require('../models/db').client
 router.get('/:c_id', async (req, res) => {
     const {c_id} = req.params;
     const chapter_list = `Select chapter_no, chapter_name from Chapter where course_id = $1`;
-    const c_name = `Select course_name from Course where course_id = $1`
+    const c_name_query = `Select course_name from Course where course_id = $1`
     
     try{
         const chapters = await client.query(chapter_list,[c_id]);
-        const chap_name = await client.query(c_name,[c_id]);
-
+        const course_name = await client.query(c_name_query,[c_id]);
         res.render('course_page', {
             course_id : c_id,
+            c : course_name.rows[0],
             chapter : chapters.rows,
-            chap_name : chap_name.rows[0]
         })
     }
     catch (e) { console.error(e.message); }
@@ -23,16 +22,18 @@ router.get('/:c_id', async (req, res) => {
 
 router.get('/TA/:c_id', async (req, res) => {
     const {c_id} = req.params;
-    const ta_list = `select user_name from Users
+    const ta_list = `select user_name, roll_no from Users
                     join Teaches on Users.user_id = Teaches.user_id
                     where authorization_level = 'TA' and course_id = $1`;
+    const c_name_query = `Select course_name from Course where course_id = $1`
 
     try{
 
         const tas = await client.query(ta_list,[c_id]);
-
+        const course_name = await client.query(c_name_query,[c_id]);
         res.render('course_TA', {
             course_id : c_id,
+            c : course_name.rows[0],
             tas : tas.rows
         })
     }
